3e3e9e3638b5d44dc1e1a65109b20a493acfa990
Shohei Nagamura <shohei.nagamura@gmail.com>
2025-04-07 01:29:41 +0900

    【課題2】記事詳細ページの作成

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Article from "./components/Article";
 import Header from "./components/Header";
+import ArticleDetail from "./components/ArticleDetail";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Article />
+
+      <Routes>
+        <Route path="/" element={<Article />} />
+        <Route path="/posts/:id" element={<ArticleDetail />} />
+      </Routes>
     </div>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <header className="w-full">
       <div className="flex justify-between items-center bg-gray-800 text-white  px-8 py-6">
-        <h1 className="font-bold text-xl">Blog</h1>
+        <Link to="/">
+          <h1 className="font-bold text-xl">Blog</h1>
+        </Link>
         <ul className="font-bold text-xl">
           <li>お問い合わせ</li>
         </ul>
